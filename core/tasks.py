6528f2b9d129785bab57723363bd0955b501f587
Arthur <o.artur.2001@gmail.com>
2026-04-12 14:24:38 +0300

    Добавлен класс Task и методы complete и update_title

--- core/tasks.py
+++ core/tasks.py
@@ -1,21 +1,48 @@
 import datetime
 
-def add_task(tasks: dict[int, dict], title: str, description: str | None = None) -> dict:
+
+class Task:
+    def __init__(self, title: str, description: str | None = None) -> None:
+        self.id = 1
+        self.title = title
+        self.description = description
+        self.is_completed = False
+        self.created_at = datetime.datetime.now()
+
+    def complete(self) -> None:
+        self.is_completed = True
+
+    def update_title(self, new_title: str) -> None:
+        self.title = new_title
+
+
+def add_task(
+    tasks: dict[int, dict], title: str, description: str | None = None
+) -> dict:
     task_id = len(tasks) + 1
     created_at = datetime.datetime.now()
-    dict_task = {'id': task_id, 'title': title, 'description': description, 'is_completed': False, 'created_at': created_at}
+    dict_task = {
+        "id": task_id,
+        "title": title,
+        "description": description,
+        "is_completed": False,
+        "created_at": created_at,
+    }
     tasks[task_id] = dict_task
     return dict_task
+
 
 def list_tasks(tasks: dict[int, dict]) -> list[dict]:
     return list(tasks.values())
 
+
 def complete_task(tasks: dict[int, dict], task_id: int) -> dict | None:
     try:
-        tasks[task_id]['is_completed'] = True
+        tasks[task_id]["is_completed"] = True
         return tasks[task_id]
     except KeyError:
         return None
+
 
 def delete_task(tasks: dict[int, dict], task_id: int) -> dict | None:
     try:
--- main.py
+++ main.py
@@ -1,58 +1,60 @@
 from core.tasks import add_task, delete_task, complete_task, list_tasks
 
+
 def main():
     tasks = {}
     while True:
-        print('1. Добавить задачу')
-        print('2. Показать список задач')
-        print('3. Отметить задачу выполненной')
-        print('4. Удалить задачу')
-        print('5. Выход')
-        choice = input('Выберите действие: ')
+        print("1. Добавить задачу")
+        print("2. Показать список задач")
+        print("3. Отметить задачу выполненной")
+        print("4. Удалить задачу")
+        print("5. Выход")
+        choice = input("Выберите действие: ")
 
-        if choice == '5':
+        if choice == "5":
             break
-        elif choice == '1':
-            title = input('Введите название задачи: ')
-            description = input('Введите описание задачи (необязательно): ')
+        elif choice == "1":
+            title = input("Введите название задачи: ")
+            description = input("Введите описание задачи (необязательно): ")
             new_task = add_task(tasks, title, description)
             print(f"Добавлена задача #{new_task['id']} - {new_task['title']}")
 
-        elif choice == '2':
+        elif choice == "2":
             all_tasks = list_tasks(tasks)
             if len(all_tasks) == 0:
-                print('Задач пока нет')
+                print("Задач пока нет")
             else:
                 for task in all_tasks:
-                    status = '✔' if task['is_completed'] else ' '
+                    status = "✔" if task["is_completed"] else " "
                     print(f"{status} №{task['id']}: {task['title']}")
-        
-        elif choice == '3':
+
+        elif choice == "3":
             try:
-                task_id = int(input('Введите номер выполненной задачи: '))
+                task_id = int(input("Введите номер выполненной задачи: "))
             except ValueError:
-                print('Введите корректный id')
+                print("Введите корректный id")
                 continue
             new_complete_task = complete_task(tasks, task_id)
             if new_complete_task is None:
-                print('Задача не найдена')
+                print("Задача не найдена")
             else:
                 print(f"✔ Задача {new_complete_task['id']} - выполнена!")
-            
-        elif choice == '4':
+
+        elif choice == "4":
             try:
-                task_id = int(input('Введите номер удаляемой задачи: '))
+                task_id = int(input("Введите номер удаляемой задачи: "))
             except ValueError:
-                print('Введите корректный id')
+                print("Введите корректный id")
                 continue
             new_delete_task = delete_task(tasks, task_id)
             if new_delete_task is None:
-                print('Задача не найдена')
+                print("Задача не найдена")
             else:
                 print(f"Задача {new_delete_task['id']} - удалена!")
 
         else:
-            print('Такого действия нет')
+            print("Такого действия нет")
 
-if __name__ == "__main__": 
+
+if __name__ == "__main__":
     main()
